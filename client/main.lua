f1760440b51c85b6630b67bb02dd8974abc62165
Codexis <85877781+CodexisPhantom@users.noreply.github.com>
2025-08-31 11:25:00 +0200

    fix(client): menu not closing

--- client/main.lua
+++ client/main.lua
@@ -26,6 +26,7 @@ local currentMenu
 local menuChanged
 local menuHistory = {}
 local nearbyZones
+local flag = 26
 
 -- Toggle ox_target, instead of holding the hotkey
 local toggleHotkey = GetConvarInt('ox_target:toggleHotkey', 0) == 1
@@ -56,21 +57,19 @@ local function shouldHide(option, distance, endCoords, entityHit, entityType, en
         return true
     end
 
+    if not option.me and entityHit == cache.ped then
+        return true
+    end
+
     local bone = entityModel and option.bones or nil
 
     if bone then
-        ---@cast entityHit number
-        ---@cast entityType number
-        ---@cast entityModel number
-
         local _type = type(bone)
 
         if _type == 'string' then
             local boneId = GetEntityBoneIndexByName(entityHit, bone)
 
-            if boneId ~= -1 and #(endCoords - GetEntityBonePosition_2(entityHit, boneId)) <= 2 then
-                bone = boneId
-            else
+            if boneId == -1 or #(endCoords - GetEntityBonePosition_2(entityHit, boneId)) > 2 then
                 return true
             end
         elseif _type == 'table' then
@@ -89,9 +88,7 @@ local function shouldHide(option, distance, endCoords, entityHit, entityType, en
                 end
             end
 
-            if closestBone then
-                bone = closestBone
-            else
+            if not closestBone then
                 return true
             end
         end
@@ -100,10 +97,6 @@ local function shouldHide(option, distance, endCoords, entityHit, entityType, en
     local offset = entityModel and option.offset or nil
 
     if offset then
-        ---@cast entityHit number
-        ---@cast entityType number
-        ---@cast entityModel number
-
         if not option.absoluteOffset then
             local min, max = GetModelDimensions(entityModel)
             offset = (max - min) * offset + min
@@ -116,14 +109,12 @@ local function shouldHide(option, distance, endCoords, entityHit, entityType, en
         end
     end
 
-    if not option.me and entityHit == cache.ped then
-        return true
-    end
-
     if option.canInteract then
         local success, resp = pcall(option.canInteract, entityHit, distance, endCoords, option.name, bone)
         return not success or not resp
     end
+
+    return false
 end
 
 local disablePunching = false
@@ -239,7 +230,7 @@ local function startTargeting()
         nearbyZones, zonesChanged = utils.getNearbyZones(endCoords)
 
         local entityChanged = entityHit ~= lastEntity
-        local newOptions = (zonesChanged or entityChanged or menuChanged) and true
+        local newOptions = (zonesChanged or entityChanged or menuChanged)
 
         if entityHit > 0 and entityChanged then
             currentMenu = nil
@@ -254,18 +245,19 @@ local function startTargeting()
             end
         end
 
-        if hasTarget and (zonesChanged or entityChanged and hasTarget > 1) then
-            if entityChanged then options:wipe() end
-            if debug and lastEntity > 0 then SetEntityDrawOutline(lastEntity, false) end
-
+        if hasTarget and (zonesChanged or (entityChanged and hasTarget > 1)) then
+            if entityChanged then 
+                options:wipe() 
+            end
+            if debug and lastEntity > 0 then 
+                SetEntityDrawOutline(lastEntity, false) 
+            end
             hasTarget = false
         end
 
         local shouldReloadOptions = newOptions and entityModel and entityHit > 0
-        if shouldReloadOptions then
-            if entityType ~= 1 or entityChanged then
-                options:set(entityHit, entityType, entityModel)
-            end
+        if shouldReloadOptions and (entityType ~= 1 or entityChanged) then
+            options:set(entityHit, entityType, entityModel)
         end
 
         if lastEntity ~= entityHit then
@@ -283,7 +275,7 @@ local function startTargeting()
         for k, v in pairs(options) do
             local optionCount = #v
             local dist = k == '__global' and 0 or distance
-            totalOptions += optionCount
+            totalOptions = totalOptions + optionCount
 
             for i = 1, optionCount do
                 local option = v[i]
@@ -294,16 +286,20 @@ local function startTargeting()
                     newOptions = true
                 end
 
-                if hide then hidden += 1 end
+                if hide then 
+                    hidden = hidden + 1 
+                end
             end
         end
 
-        if zonesChanged then table.wipe(zones) end
+        if zonesChanged then 
+            table.wipe(zones) 
+        end
 
         for i = 1, #nearbyZones do
             local zoneOptions = nearbyZones[i].options
             local optionCount = #zoneOptions
-            totalOptions += optionCount
+            totalOptions = totalOptions + optionCount
             zones[i] = zoneOptions
 
             for j = 1, optionCount do
@@ -315,7 +311,9 @@ local function startTargeting()
                     newOptions = true
                 end
 
-                if hide then hidden += 1 end
+                if hide then 
+                    hidden = hidden + 1 
+                end
             end
         end
 
@@ -328,17 +326,16 @@ local function startTargeting()
                 if hasTarget and hasTarget ~= 1 then
                     hasTarget = false
                 end
-            elseif menuChanged or hasTarget ~= 1 and hidden ~= totalOptions then
+            elseif menuChanged or (hasTarget ~= 1 and hidden ~= totalOptions) then
                 hasTarget = options.size
-                if currentMenu and options.__global[1]?.name ~= 'builtin:goback' then
-                    table.insert(options.__global, 1,
-                        {
-                            icon = 'fa-solid fa-circle-chevron-left',
-                            label = locale('go_back'),
-                            name = 'builtin:goback',
-                            menuName = currentMenu,
-                            openMenu = 'home'
-                        })
+                if currentMenu and options.__global[1] and options.__global[1].name ~= 'builtin:goback' then
+                    table.insert(options.__global, 1, {
+                        icon = 'fa-solid fa-circle-chevron-left',
+                        label = locale('go_back'),
+                        name = 'builtin:goback',
+                        menuName = currentMenu,
+                        openMenu = 'home'
+                    })
                 end
             end
 
@@ -416,11 +413,20 @@ local function getResponse(option, server)
     return response
 end
 
-RegisterNUICallback('close', function(_, cb)
-    cb(1)
+local function cleanupTarget()
     disablePunching = false
     state.setNuiFocus(false, false)
+    table.wipe(currentTarget)
+    options:wipe()
     SendNuiMessage('{"event": "visible", "state": false}')
+    if nearbyZones then 
+        table.wipe(nearbyZones) 
+    end
+end
+
+RegisterNUICallback('close', function(_, cb)
+    cb(1)
+    cleanupTarget()
 end)
 
 RegisterNUICallback('select', function(data, cb)
@@ -428,64 +434,69 @@ RegisterNUICallback('select', function(data, cb)
 
     disablePunching = false
 
-    if data[2] == 0 then
-        state.setNuiFocus(false, false)
-        SendNuiMessage('{"event": "visible", "state": false}')
+    if not data or #data < 2 or data[2] == 0 then
+        cleanupTarget()
+        return
     end
 
-    local zone = data[3] and nearbyZones[data[3]]
+    local zone = data[3] and nearbyZones and nearbyZones[data[3]]
+    local option = zone and zone.options[data[2]] or (data[1] and options[data[1]] and options[data[1]][data[2]])
 
-    ---@type OxTargetOption?
-    local option = zone and zone.options[data[2]] or options[data[1]][data[2]]
+    if not option then
+        cleanupTarget()
+        return
+    end
 
-    if option then
-        if option.openMenu then
-            local menuDepth = #menuHistory
+    local shouldCleanup = true
 
-            if option.name == 'builtin:goback' then
-                option.menuName = option.openMenu
-                option.openMenu = menuHistory[menuDepth]
+    if option.openMenu then
+        local menuDepth = #menuHistory
 
-                if menuDepth > 0 then
-                    menuHistory[menuDepth] = nil
-                end
-            else
-                menuHistory[menuDepth + 1] = currentMenu
+        if option.name == 'builtin:goback' then
+            option.menuName = option.openMenu
+            option.openMenu = menuHistory[menuDepth]
+
+            if menuDepth > 0 then
+                menuHistory[menuDepth] = nil
             end
-
-            menuChanged = true
-            currentMenu = option.openMenu ~= 'home' and option.openMenu or nil
-
-            options:wipe()
         else
-            state.setNuiFocus(false)
+            menuHistory[menuDepth + 1] = currentMenu
         end
 
-        currentTarget.zone = zone?.id
-
-        if option.onSelect then
-            option.onSelect(option.qtarget and currentTarget.entity or getResponse(option))
-        elseif option.export then
-            exports[option.resource or zone.resource][option.export](nil, getResponse(option))
-        elseif option.event then
-            TriggerEvent(option.event, getResponse(option))
-        elseif option.serverEvent then
-            TriggerServerEvent(option.serverEvent, getResponse(option, true))
-        elseif option.command then
-            ExecuteCommand(option.command)
-        end
-
-        if option.menuName == 'home' then return end
+        menuChanged = true
+        currentMenu = option.openMenu ~= 'home' and option.openMenu or nil
+        options:wipe()
+        shouldCleanup = false
+    else
+        state.setNuiFocus(false)
     end
 
-    if not option?.openMenu and IsNuiFocused() then
+    currentTarget.zone = zone and zone.id
+
+    if option.onSelect then
+        option.onSelect(option.qtarget and currentTarget.entity or getResponse(option))
+    elseif option.export then
+        exports[option.resource or zone.resource][option.export](nil, getResponse(option))
+    elseif option.event then
+        TriggerEvent(option.event, getResponse(option))
+    elseif option.serverEvent then
+        TriggerServerEvent(option.serverEvent, getResponse(option, true))
+    elseif option.command then
+        ExecuteCommand(option.command)
+    end
+
+    if option.menuName == 'home' then
+        shouldCleanup = false
+    end
+
+    if not (option and option.openMenu) and IsNuiFocused() then
         state.setActive(false)
     end
 
-    state.setNuiFocus(false, false)
-    table.wipe(currentTarget)
-    options:wipe()
-    SendNuiMessage('{"event": "visible", "state": false}')
-
-    if nearbyZones then table.wipe(nearbyZones) end
+    if shouldCleanup then
+        cleanupTarget()
+    else
+        state.setNuiFocus(false, false)
+        SendNuiMessage('{"event": "visible", "state": false}')
+    end
 end)
